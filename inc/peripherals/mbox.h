82abe1132a452ea979652f7da3e1362df288e9f5
Hakula Chen <i@hakula.xyz>
2021-01-23 03:05:43 +0800

    style: fix header guard

--- inc/peripherals/mbox.h
+++ inc/peripherals/mbox.h
@@ -1,9 +1,9 @@
-#ifndef INC_PERIPHERALS_MBOX_H
-#define INC_PERIPHERALS_MBOX_H
+#ifndef INC_PERIPHERALS_MBOX_H_
+#define INC_PERIPHERALS_MBOX_H_
 
 #include "peripherals/base.h"
 
 int mbox_get_arm_memory();
 int mbox_get_clock_rate();
 
-#endif
+#endif  // INC_PERIPHERALS_MBOX_H_
