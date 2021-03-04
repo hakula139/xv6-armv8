ce1693f7768fd150b1c3f1fad5fcce584930d70a
Hakula Chen <i@hakula.xyz>
2021-03-04 22:00:20 +0800

    feat: cat

--- user/src/cat/main.c
+++ user/src/cat/main.c
@@ -1,7 +1,38 @@
+#include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
+
+char buf[512];
+
+void
+cat(int fd)
+{
+    int n = read(fd, buf, sizeof(buf));
+    while (n > 0) {
+        if (write(1, buf, n) != n) {
+            printf("cat: write error.\n");
+            return;
+        }
+    }
+    if (n < 0) {
+        printf("cat: read error.\n");
+        return;
+    }
+}
 
 int
 main(int argc, char* argv[])
 {
-    /* TODO: Your code here. */
+    if (argc <= 1) _exit(-1);
+
+    for (int i = 1; i < argc; ++i) {
+        int fd = open(argv[i], 0);
+        if (fd < 0) {
+            printf("cat: cannot open %s.\n", argv[i]);
+            _exit(-1);
+        }
+        cat(fd);
+        close(fd);
+    }
+    _exit(0);
 }
